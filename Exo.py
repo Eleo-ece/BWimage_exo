f9cb480c906e0fe179757486d4523a3fb1c8a53b
Eleo <rib_ele@outlook.fr>
2021-01-05 16:49:40 +0100

    new function

--- Exo.py
+++ Exo.py
@@ -1,9 +1,6 @@
 #importing an image with opencv
 import cv2
 
-#read image in color
-image=cv2.imread('jon.jpg')
-
 #convert to black and white image
 gray=cv2.imread('jon.jpg', 0) 
 
@@ -14,3 +11,15 @@ cv2.destroyAllWindows()
 
 #save image
 cv2.imwrite('jonBW.jpg', gray)
+
+#new function
+gray=cv2.split(gray)[0]
+(retVal, newImg)=cv2.threshold(gray, 130, 255, cv2.THRESH_BINARY)
+
+#display new image
+cv2.imshow("new", newImg)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+#save new image
+cv2.imwrite('jonNew.jpg', newImg)
